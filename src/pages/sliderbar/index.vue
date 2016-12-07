12a0f55b9cb959537a1241ca847b2ec247a4e466
Lagou <lagou@LagoudeMac-mini-2.local>
2016-12-07 10:06:04 +0800

    modify config.js

--- src/pages/sliderbar/index.vue
+++ src/pages/sliderbar/index.vue
@@ -2,14 +2,14 @@
   <el-col :span="3">
     <el-menu default-active="2" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose">
       <el-submenu index="1">
-        <template slot="title"><i class="el-icon-message"></i>导航i</template>
+        <template slot="title"><i class="el-icon-message"></i>导航</template>
         <el-menu-item-group>
-          <template slot="title">分组一</template>
+          <template slot="title">分组</template>
           <el-menu-item index="1-1">选项</el-menu-item>
           <el-menu-item index="1-2">选项</el-menu-item>
         </el-menu-item-group>
-        <el-menu-item-group title="分组2">
-          <el-menu-item index="1-3">选项3</el-menu-item>
+        <el-menu-item-group title="分组">
+          <el-menu-item index="1-3">选项</el-menu-item>
         </el-menu-item-group>
       </el-submenu>
       <el-menu-item index="2"><i class="el-icon-menu"></i>导航</el-menu-item>
@@ -21,7 +21,6 @@
 export default {
     methods: {
         handleOpen(key, keyPath) {
-          console.log('aaaa');
             console.log(key, keyPath);
         },
         handleClose(key, keyPath) {
