d63cc0b291bdd97ab93af37618b525f0e93d6989
alisa <alisa@alisadeMacBook-Pro.local>
2016-12-11 09:40:44 +0800

    修改build配置文件

--- config/prod.js
+++ config/prod.js
@@ -1,4 +1,5 @@
 const ExtractTextPlugin = require("extract-text-webpack-plugin"),
+    webpack = require('webpack'),
     HtmlWebpackPlugin = require('html-webpack-plugin');
 
 
@@ -14,12 +15,22 @@ module.exports = {
     },
     devtool: 'source-map',
     plugins: [
+        new webpack.DefinePlugin({
+            'process.env': {
+                'NODE_ENV': JSON.stringify('production')
+            }
+        }),
         new ExtractTextPlugin({
             filename: './static/css/styles-[hash].css',
             allChunk: true
         }),
         new HtmlWebpackPlugin({ //根据模板插入css/js等生成最终HTML
             template: './index.html' //html模板路径
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false
+            }
         })
     ],
 };
